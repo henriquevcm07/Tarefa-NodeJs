0df1db6ea511f298adcb3c60be51d993a0f75737
Henrique Veloso <@henriquevcm07@gmail.com>
2026-07-30 14:54:26 -0300

    feat: instalando dependências

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['src'],
+    format:['esm'],
+    outDir:'build',
+    sourcemap: true,
+    shims: true,
+    target: 'esnext'})
