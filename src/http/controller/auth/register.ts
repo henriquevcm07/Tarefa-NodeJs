e4a5e5a7308dfc3cebb581a7dd12b63a464284f9
Henrique Veloso <@henriquevcm07@gmail.com>
2026-07-31 19:07:45 -0300

    fix: arrumando o register

--- src/http/controller/auth/register.ts
+++ src/http/controller/auth/register.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { prisma } from '@/libs/prisma.js';
+import { hash } from 'bcryptjs';
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function register(request: FastifyRequest, reply: FastifyReply){
@@ -7,15 +8,27 @@ export async function register(request: FastifyRequest, reply: FastifyReply){
         name: z.string().trim().min(1).max(100), 
         email: z.email().max(100),
         password: z.string().min(8).max(100),
+        role: z.enum(['admin', 'user']).default('user'),
     })
 
-    const {name, email, password} = registerBodySchema.parse(request.body)
+    const {name, email, password, role} = registerBodySchema.parse(request.body)
+
+    const hashedPassword = await hash(password, 10)
 
     const user = await prisma.user.create({
         data:{
             name,
             email,
-            password,
+            password: hashedPassword,
+            role
         }})
-        return reply.status(201).send(user)
+    const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+    }
+    return reply.status(201).send(userWithoutPassword)
 }
