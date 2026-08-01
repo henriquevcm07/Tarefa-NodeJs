7f456f124c22f41aed6c75d9cdc1e1f8c9730dc9
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-01 14:25:52 -0300

    feat: getTask implementado

---
+++ src/http/controller/tasks/getTask.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/libs/prisma.js';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function getTask(request: FastifyRequest, reply: FastifyReply){
+    const { id: taskId } = request.params as { id: string }
+    const task = await prisma.task.findUnique({
+        where: {
+            id: Number(taskId),
+        },
+        include: {
+            tasksUsers: {
+                select: {
+                    userId: true, 
+                    name: true,
+                    email: true,
+                    role: true,
+                }
+
+        },
+    }})
+    if (!task) {
+        return reply.status(404).send({ error: 'Tarefa não encontrada' })
+    }
+    const taskResponse = {
+        id: task.id,
+        description: task.description,
+        priority: task.priority,
+        completed: task.completed,
+        deadline: task.deadline,
+        projectId: task.projectId,
+        createdAt: task.createdAt,
+        updatedAt: task.updatedAt,
+        assignedUsers: task.tasksUsers.map((taskUser) => ({
+            Id: taskUser.userId,
+            name: taskUser.name,
+            email: taskUser.email,
+            role: taskUser.role,
+        })),
+    }
+    return reply.status(200).send(taskResponse)
+}
--- src/http/controller/tasks/tasks.routes.ts
+++ src/http/controller/tasks/tasks.routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify';
 import { listTasks } from './listTasks.js';
 import { verifyJwt } from '@/http/middleware/verify-jwt.js';
+import { getTask } from './getTask.js';
 
 export async function tasksRoutes(app: FastifyInstance){
-        app.get('/listTasks', {onRequest: [verifyJwt]}, listTasks)
+        app.get('/', {onRequest: [verifyJwt]}, listTasks)
+        app.get('/:id', {onRequest: [verifyJwt]}, getTask)
 }
