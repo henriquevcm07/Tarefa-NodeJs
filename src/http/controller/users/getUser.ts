7feb89b1c4a4859e176d0e8d2dd09373223b17f8
Henrique Veloso <@henriquevcm07@gmail.com>
2026-07-31 22:14:18 -0300

    feat: procura de usuario por Id

---
+++ src/http/controller/users/getUser.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/libs/prisma.js';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+export async function getUser(request: FastifyRequest, reply: FastifyReply){
+    const getIdParamsSchema = z.object({
+        id: z.coerce.number()
+    })
+    const { id } = getIdParamsSchema.parse(request.params)
+
+    const user = await prisma.user.findUnique({
+        where:{
+            id
+        }
+    })
+    if (!user) {
+        return reply.status(404).send({ message: 'Usuário não encontrado' })
+    }
+    return reply.status(200).send(user)
+}
--- src/http/controller/users/users.routes.ts
+++ src/http/controller/users/users.routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify';
 import { verifyJwt } from '@/http/middleware/verify-jwt.js';
 import { listUsers } from './listUsers.js';
+import { getUser} from './getUser.js';
 
 export async function usersRoutes(app: FastifyInstance){
-        app.get('/listUsers', {onRequest: [verifyJwt]}, listUsers)
+        app.get('/', {onRequest: [verifyJwt]}, listUsers)
+        app.get('/:id', {onRequest: [verifyJwt]}, getUser)
 }
