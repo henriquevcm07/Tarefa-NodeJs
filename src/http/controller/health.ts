b2cdf0bfd2a7584a52005b0562042495d48158a1
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-02 00:10:37 -0300

    fix: consertando a rota do health

--- src/http/controller/health.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from '@/libs/prisma.js';
-import type { FastifyReply, FastifyRequest } from 'fastify';
-
-export async function health(_request: FastifyRequest, reply: FastifyReply){
-    return reply.status(200).send({ message: 'API is healthy' })
-}
--- src/http/controller/routes.ts
+++ src/http/controller/routes.ts
@@ -4,7 +4,6 @@ import { projectsRoutes } from './projects/projects.routes.js';
 import { usersRoutes } from './users/users.routes.js';
 import { tasksRoutes } from './tasks/tasks.routes.js';
 import { reportsRoutes } from './reports/reports.route.js';
-import { health } from './health.js';
 
 export async function appRoutes(app: FastifyInstance){
     app.register(authRoutes, { prefix: '/auth'  })
@@ -12,5 +11,7 @@ export async function appRoutes(app: FastifyInstance){
     app.register(usersRoutes, { prefix: '/users' })
     app.register(tasksRoutes, { prefix: '/tasks' })
     app.register(reportsRoutes, { prefix: '/reports' })
-    app.get('/health', health)
+    app.get('/health', async (request, reply) => {
+    return reply.status(200).send({ status: 'ok' })
+})
 }
