4ea4c7e113936addab0851f823ae51bae8107500
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-01 23:25:54 -0300

    fix: ajustando o app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import fastify from 'fastify';
-import { env } from '@/env/index.js';
-import { appRoutes } from './http/controller/routes.js';
-import fastifyJwt from '@fastify/jwt'; 
-import cors from '@fastify/cors';
+import fastify from 'fastify'
+import { env } from './env/index.js'
+import { appRoutes } from './http/controller/routes.js'
+import fastifyJwt from '@fastify/jwt';
+import cors from '@fastify/cors'
 
 export const app = fastify()
 
@@ -10,7 +10,7 @@ app.register(fastifyJwt, {
     secret: env.JWT_SECRET
 })
 
-await app.register(cors, {
+app.register(cors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 })
